7c641bf1f66cf593bf5c7c32ec4dae3af2b4c80a
Mohammed Bilal Sharief Chisti <149594531+BilalChisti12@users.noreply.github.com>
2026-09-13 19:54:44 +0530

    7 ms (96.01%) Medium String

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning-09-13-2026-19-54-43.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void f(int i, String s, StringBuilder sb, List<List<String>> res, List<String> temp){
+        if(i == s.length()){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int j=i; j<s.length(); j++){
+            if(isPal(s, i, j)){
+                temp.add(s.substring(i, j+1));
+                f(j+1, s, sb, res, temp);
+                temp.removeLast();
+            }
+        }
+    }
+    public boolean isPal(String s, int l, int r){
+        if(s.length() == 1) return true;
+        while(l<r){
+            if(s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        f(0, s, new StringBuilder(), res, new ArrayList<>());
+        return res;
+    }
+}
